07b4085e9388d2e1894bb44f97160702f5938eaa
Mavinkip <mavinngetich1@gmail.com>
2023-05-06 17:02:16 -0700

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[from:([A-Za-z0-9]+)\] \[to:([A-Za-z0-9]+)\] \[flags:([\d:-]+)\]/).join
+puts ARGV[0].scan(^from:[A-Za-z0-9]to:[A-Za-z0-9]flag:[\d{4}(0|1|-1]$)
