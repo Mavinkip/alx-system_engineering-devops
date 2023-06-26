9d033dfdf7a91a949ff14c0d84fc4592b838a2fb
Mavinkip <mavinngetich1@gmail.com>
2023-06-26 04:42:42 +0300

    5

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 #connecting
-file_line { 'configuration_file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '	IdentityFile ~/.ssh/school',
-}
-file_line { 'no_password':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '	PasswordAuthentication no',
+file { '/home/100.26.221.191/.ssh/config':
+  ensure => present,
+  mode   => '0600',
+  content => "
+    Host 100.26.221.191
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
